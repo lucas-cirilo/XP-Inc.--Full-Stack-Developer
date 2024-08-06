6a619d366c34726c0d173feeb93340fcf3554a9c
Lucas Cirilo <llcs0595@gmail.com>
2024-08-05 22:09:15 -0300

    Updating Fundamentos 2

---
+++ Fundamentos - 02./Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Fundamentos.Models;
+
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(98, 30);
+calc.Multiplicar(10, 30);
+calc.Dividir(50, 2);
+calc.Potencia(3, 3);
+calc.Seno(45);
+calc.Cosseno(45);
+calc.Tangente(45);
+calc.RaizQuadrada(9);
+
+
+
+
+
+
+
+// Incremento do número 10
+// outra opção seria numero +1;
+
+int numero =10;
+numero = numero +1;  // outra opção numero++;
+Console.WriteLine(numero);
+
+// Decremento do número 9
+
+int decremento = 9;
+decremento--;
+
+Console.WriteLine(decremento);
