d4f5de411097ed13b47ee7f8dde32b65ed6d4218
Lucas Cirilo <llcs0595@gmail.com>
2024-08-13 14:17:11 -0300

    updating paste 02

---
+++ 02 - Estrutura de um programa/02 - Estrutura/Estrutura.Common/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Estrutura.Common;
+
+public class Class1
+{
+}
---
+++ 02 - Estrutura de um programa/02 - Estrutura/Estrutura/Program.cs
@@ -0,0 +1,12 @@
+﻿using Estrutura.Common.Models;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 78;
+arrayInteiros[2] = 120;
+
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($" Posição Nº {contador} - {arrayInteiros[contador]}");
+}
