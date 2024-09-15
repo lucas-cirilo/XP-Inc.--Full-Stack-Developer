3778410a02bc22d01e62b0d9ff9b8a899197f4da
Lucas Cirilo <llcs0595@gmail.com>
2024-09-15 14:26:18 -0300

    adding directory 9

---
+++ 03 - Dados e Listas com .Net C#/09 - Implementado Construtores /Program.cs
@@ -0,0 +1,14 @@
+﻿using _09_Implementando_Construtores.Models;
+
+Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Santos"); // usando o construtor
+Pessoa p2 = new Pessoa(nome:"Juliana", sobrenome: "Oliveira");
+Pessoa p3 = new Pessoa(nome: "Thays", sobrenome: "Moura");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
+cursoDeIngles.ListarAlunos();
