377c6765c3ff234782a81b4c0e97d6b2fd17c4ba
Lucas Cirilo <llcs0595@gmail.com>
2024-09-20 18:29:12 -0300

    Adding new file

---
+++ 05 - Exceções e Coleções/08 - Pilhas/Program.cs
@@ -0,0 +1,22 @@
+﻿using _08_Pilhas.Models;
+using System.Globalization;
+
+Stack<int> pilha = new Stack<int>(); // cria uma pilha de inteiros
+
+pilha.Push(1); // empilha o elemento 1
+pilha.Push(2); // empilha o elemento 2
+pilha.Push(3); // empilha o elemento 3
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item); // exibe os elementos da pilha (1, 2, 3)
+}
+
+Console.WriteLine($"Removendo o elemento do topo da pilha: {pilha.Pop()}"); // remove o elemento do topo da pilha (3)
+
+pilha.Push(4); // empilha o elemento 4
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item); // exibe os elementos da pilha
+}
